23229873515a87f6c11792a9a78510609db95c99
CrazyPancakeMuncher08 <70578309+CrazyPancakeMuncher08@users.noreply.github.com>
2020-09-16 09:34:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,15 @@ var ranktext = document.getElementById("ranktext")
 var nameinput = document.getElementById("nameinput")
 var nametext = document.getElementById("nametext")
 var bginput = document.getElementById("backgroundinput")
+var inputfile = document.getElementById("inputfile")
 var gurka_rot = 0
 var gurka_rot_destination = 0
 var gurkor = 0
 var gpc = 1
+var code = "2021"
+var autocn = false
+var securitybypass = true
+var menabled = false
 var upgradeprice = 12
 var autoclickers = 0
 var rank = 1
@@ -17,6 +22,27 @@ var background = ""
 
 var skins = []
 var ownedskin = null
+
+var lastinputValue = 0
+var lastgpc = 0
+var lastdoubled = 0
+var lastautoclickers = 0
+
+var lastgurkorsaved = 0
+var lastgpcsaved = 0
+var lastdoubledsaved = 0
+var lastautoclickerssaved = 0
+
+function ResetCode() {
+    if (securitybypass == false) {
+        code = ""
+    }
+}
+
+function waitenable() {
+    menabled = true
+    console.log("M_AntiCheat enabled!")
+}
 
 function lerp(v0, v1, t) {
     return v0*(1-t)+v1*t
@@ -33,10 +59,15 @@ class Skin {
   }
   buy() {
       if (gurkor >= this.price) {
+          code = 2041
+          lastinputValue = gurkor
           gurkor -= this.price
           ownedskin = this
           gurkortext.innerHTML = "Gurkor: " + gurkor.toString()
           gurka.src = this.path
+          this.resize()
+          lastinputValue = gurkor
+          setTimeout(ResetCode,500)
       }
   }
   resize() {
@@ -67,19 +98,46 @@ var milkskin = new Skin("Milk",300000,"milk.png",300,300)
 var fortniteskin = new Skin("Fortnite",50000,"fortnite.png",300,300)
 var thicksteveskin = new Skin("Thick Steve",9000000000000,"thicksteve.png",400,300)
 var nuggetskin = new Skin("Nugget",900000000000000,"nugget.png",300,300)
+var turtleskin = new Skin("Turtle",90000000000,"turtle.png",400,300)
+var solskin = new Skin("Sol",9000000,"sol.png",300,300)
 var wrongwayskin = new Skin("Wrong Way",0,"wrongwaygurka.png",300,300)
 var gskin = new Skin("G",0,"g.png",300,300)
 
 ownedskin = deafultskin
 
+var lastClick = 0
+function DetectAutoclicker() {
+    if (securitybypass == false) {
+        var d = new Date()
+        var t = d.getTime()
+        if (lastClick < t &&  lastClick > t - 100) {
+            autocn = true
+            setTimeout(ResetAutocn,500)
+            console.log("Autoclicker detected!")
+        }
+        lastClick = t
+    }
+}
+
+function ResetAutocn() {
+    autocn = false
+}
+
 function Click() {
-    gurkor += gpc
-    if (ownedskin != wrongwayskin) {
-        gurka_rot_destination += gpc
-    } else {
-        gurka_rot_destination -= gpc
+    DetectAutoclicker()
+    code = 2041
+    lastinputValue = gurkor
+    if (autocn == false) {
+        gurkor += gpc
+        if (ownedskin != wrongwayskin) {
+            gurka_rot_destination += gpc
+        } else {
+            gurka_rot_destination -= gpc
+        }
     }
     UpdateGurka()
+    lastinputValue = gurkor
+    setTimeout(ResetCode,500)
 }
 
 function UpdateGurka() {
@@ -97,6 +155,7 @@ function UpdateGurka() {
 }
 
 function Save() {
+    code = 2041
     localStorage.setItem("bg",background)
     localStorage.setItem("name",name)
     localStorage.setItem("doubled",doubled)
@@ -106,9 +165,42 @@ function Save() {
     localStorage.setItem("skin",ownedskin.name)
     localStorage.setItem("gpc",gpc)
     localStorage.setItem("gurkor",gurkor)
+    setTimeout(ResetCode,500)
+}
+
+function SaveFile() {
+    download(gurkor + "|" + gpc + "|" + doubled + "|" + upgradeprice + "|" + autoclickers + "|" + rank + "|" + name + "|" + background + "|" + ownedskin.name, "game.save", "text/plain")
+}
+
+function LoadFile() {
+    var inputfile = document.getElementById("inputfile")
+    var files = inputfile.files
+    if(files.length == 0) return;
+    const file = files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+        const file = e.target.result;
+        const lines = file.split("|");
+        gurkor = parseInt(lines[0])
+        gpc = parseInt(lines[1])
+        doubled = parseInt(lines[2])
+        upgradeprice = parseInt(lines[3])
+        autoclickers = parseInt(lines[4])
+        rank = parseInt(lines[5])
+        name = lines[6]
+        background = lines[7]
+        for (i = 0; i < skins.length; i++) {
+            if (skins[i].name == lines[8]) {
+                ownedskin = skins[i]
+            }
+        }
+    }
+    reader.readAsText(file);
+    UpdateGurka()
 }
 
 function Load() {
+    code = 2041
     background = localStorage.getItem("bg")
     name = localStorage.getItem("name")
     doubled = parseInt(localStorage.getItem("doubled"))
@@ -125,19 +217,23 @@ function Load() {
     nameinput.value = name
     bginput.value = background
     UpdateGurka()
+    setTimeout(ResetCode,500)
 }
 
 function Buy(skin) {
     skins[skin].buy()
-    skins[skin].resize()
 }
 
 function Upgrade() {
     if (gurkor >= upgradeprice) {
+        code = 2041
+        lastinputValue = gurkor
         gurkor -= upgradeprice
         autoclickers += 1
         upgradeprice += 12
         UpdateGurka()
+        lastinputValue = gurkor
+        setTimeout(ResetCode,500)
     }
 }
 
@@ -149,6 +245,8 @@ function Upgrade10() {
 
 function Rebirth() {
     if (rank == 7) {
+        code = 2041
+        lastinputValue = gurkor
         gurkor = 0
         rank = 1
         gpc += 1
@@ -158,6 +256,8 @@ function Rebirth() {
         gurka_rot_destination = 0
         ownedskin = deafultskin
         UpdateGurka()
+        lastinputValue = gurkor
+        setTimeout(ResetCode,500)
     }
 }
 
@@ -169,7 +269,6 @@ function Tick() {
     if (bginput.value != "") {
         background = bginput.value
         UpdateGurka()
-        console.log(background)
     }
     if (document.getElementById("autobbox").checked == true) {
         Upgrade()
@@ -262,17 +361,80 @@ function FiveTick() {
 
 function logKey(e) {
     console.log(e.keyCode)
-    if (e.keyCode == 71) {
-        if (gunlocked == false) {
-            gunlocked = true
-            alert("You are now an epic gurka gamer because you pressed g on your keyboard! Press ok to get an epic skin that gives you 5 gurkor every 10 seconds.")
-            ownedskin = gskin
+}
+
+function ZeroZeroTick() {
+    if (code != "2021" && menabled == true) {
+        newValue = gurkor
+        newgpc = gpc
+        newdoubled = doubled
+        newautoclickers = autoclickers
+        newgurkorsaved = parseInt(localStorage.getItem("gurkor"))
+        newautoclickerssaved = parseInt(localStorage.getItem("autoc"))
+        newgpcsaved = parseInt(localStorage.getItem("gpc"))
+        newdoubledsaved = parseInt(localStorage.getItem("doubled"))
+        if (lastinputValue != newValue) {
+            gurkor = lastinputValue
+            alert("M_AntiCheat has detected you cheating! Dont worry you wont be banned but the changes you made will not be applied.")
+        }
+        if (lastgpc != newgpc) {
+            gpc = lastgpc
+            alert("M_AntiCheat has detected you cheating! Dont worry you wont be banned but the changes you made will not be applied.")
+        }
+        if (lastdoubled != newdoubled) {
+            doubled = lastdoubled
+            alert("M_AntiCheat has detected you cheating! Dont worry you wont be banned but the changes you made will not be applied.")
+        }
+        if (lastautoclickers != newautoclickers) {
+            autoclickers = lastautoclickers
+            alert("M_AntiCheat has detected you cheating! Dont worry you wont be banned but the changes you made will not be applied.")
+        }
+        //Saves
+        if (lastgurkorsaved != newgurkorsaved) {
+            localStorage.setItem("gurkor",lastgurkorsaved)
+            alert("M_AntiCheat has detected you cheating! Dont worry you wont be banned but the changes you made will not be applied.")
+        }
+        if (lastgpc != newgpc) {
+            gpc = lastgpc
+            alert("M_AntiCheat has detected you cheating! Dont worry you wont be banned but the changes you made will not be applied.")
+        }
+        if (lastdoubled != newdoubled) {
+            doubled = lastdoubled
+            alert("M_AntiCheat has detected you cheating! Dont worry you wont be banned but the changes you made will not be applied.")
+        }
+        if (lastautoclickers != newautoclickers) {
+            autoclickers = lastautoclickers
+            alert("M_AntiCheat has detected you cheating! Dont worry you wont be banned but the changes you made will not be applied.")
         }
     }
 }
+
+function SetOlds() {
+    lastinputValue = gurkor
+    lastgpc = gpc
+    lastdoubled = doubled
+    lastautoclickers = autoclickers
+    lastgurkorsaved = parseInt(localStorage.getItem("gurkor"))
+    lastautoclickerssaved = parseInt(localStorage.getItem("autoc"))
+    lastgpcsaved = parseInt(localStorage.getItem("gpc"))
+    lastdoubledsaved = parseInt(localStorage.getItem("doubled"))
+}
+
+function BypassSecurity(key) {
+    if (key == "2021") {
+        securitybypass = true
+        code = "2021"
+    } else {
+        console.log("Wrong security bypass key!")
+    }
+}
+
+console.log("Cheated gurkor dont taste as good as normal gurkor.")
 
 document.addEventListener('keydown', logKey);
 setInterval(Autosaving,60000)
 setInterval(FiveTick,10000)
 setInterval(Tick,1000)
 setInterval(ZeroTick,1)
+setInterval(SetOlds,500)
+setInterval(ZeroZeroTick,0.03)
